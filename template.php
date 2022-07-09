fd28d2a4e5463c382061b7810d91f48ccf196a32
Poojitha Irosha <85311373+PoojithaIrosha@users.noreply.github.com>
2022-07-09 17:34:52 +0530

    Updated template.php

--- template.php
+++ template.php
@@ -13,7 +13,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Template</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Js -->
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.js"></script>
+
 </head>
 
 <body>
